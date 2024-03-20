0f73cd5c4b2db99ecb367d487b55feccb89529bb
UNANI <coder.unani@gmail.com>
2024-03-20 21:30:06 +0900

    [feat] 기본 컴포넌트들 구현

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import { PageComponent, Composable } from "./components/page/page.js";
+import ImageComponent from "./components/page/item/image.js";
+import NoteComponent from "./components/page/item/note.js";
+import TodoComponent from "./components/page/item/todo.js";
+import VideoComponent from "./components/page/item/video.js";
+import { Component } from "./components/component.js";
+
+class App {
+  private readonly page: Component & Composable;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://picsum.photos/600/300"
+    );
+    this.page.addChild(image);
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://www.youtube.com/embed/feyaQ2PW2o4?si=nqsmC5-GNZ3W0nLO"
+    );
+    this.page.addChild(video);
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    this.page.addChild(note);
+
+    const todo = new TodoComponent("Todo Title", "Todo Item");
+    this.page.addChild(todo);
+  }
+}
+
+new App(document.querySelector(".document")! as HTMLElement);
---
+++ src/components/component.ts
@@ -0,0 +1,29 @@
+interface Component {
+  attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
+}
+/**
+ * Encapsulate the HTML element creation and insertion
+ */
+class BaseComponent<T extends HTMLElement> implements Component {
+  protected readonly element: T;
+
+  constructor(htmlString: string) {
+    const template = document.createElement("template");
+    template.innerHTML = htmlString;
+    this.element = template.content.firstElementChild! as T;
+  }
+
+  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
+    parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement) {
+    if (parent !== this.element.parentElement) {
+      throw new Error("Parent mismatch");
+    }
+    parent.removeChild(this.element);
+  }
+}
+
+export { Component, BaseComponent };
